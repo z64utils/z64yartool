3d8a298a78e252d10ce4f050d3b34fbc5a3f46da
Michael Holland <45853441+z64me@users.noreply.github.com>
2025-08-03 16:55:26 -0230

    fix build scripts

--- build-debug.sh
+++ build-debug.sh
@@ -1,3 +1,3 @@
 mkdir -p bin/
-gcc -o bin/z64yartool -Og -g -Wall -Wextra -Wno-unused-function -lm -std=c99 -pedantic -Iinclude src/*.c
+gcc -o bin/z64yartool -Og -g -Wall -Wextra -Wno-unused-function -std=c99 -pedantic -Iinclude src/*.c -lm
 
